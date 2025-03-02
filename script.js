a89fdeb4686d5da42e6ab498dce85fb02452bd02
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-03-02 10:56:32 +0100

    Associate the total change to its face values

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let price = 2.77;
+let price = 9.7;
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -21,19 +21,49 @@ const moneyValue = {
   Ten: 10,
   Twenty: 20,
   Hundred: 100
-}
+};
+const moneyKeys = Object.keys(moneyValue);
+const moneyValues = Object.values(moneyValue);
 
 const calcFaceValue = (currentPrice) => {
-  const values = Object.values(moneyValue);
-  let faceValue = [];
+  const faceValues = [];
   for(i = cid.length - 1; i >= 0; i--){
-    if(currentPrice >= values[i]){
-      Math.round((currentPrice -= values[i]) * 100) / 100;
-      faceValue.push(values[i]);
+    if(currentPrice >= moneyValues[i]){
+      Math.round((currentPrice -= moneyValues[i]) * 100) / 100;
+      faceValues.push(moneyValues[i]);
       i++;
     } 
   }
-  return faceValue;
-};
+  return faceValues;
+}
 
-console.log(calcFaceValue(price));
+const addEqualFaceValues = () => {
+  const faceValues = calcFaceValue(price);
+  const change = faceValues.reduce((accumulator, faceValue) => {
+    accumulator[faceValue] = (accumulator[faceValue] || 0) + faceValue;
+    return accumulator;
+  }, {});
+  return change;
+}
+
+const sortChange = () => {
+  const change = addEqualFaceValues();
+  const sortedChange = Object.entries(change).toSorted(([a,], [b,]) => a - b);
+  return sortedChange;
+}
+
+const associateChange = () => {
+  const sortedChange = sortChange();
+
+  const namedChange = sortedChange.map((value, index) => {
+    const newObj = {};
+    for(i = 0; i < moneyValues.length; i++){
+      if(value[0] === JSON.stringify(moneyValues[i])){
+        newObj[moneyKeys[i]] = value[1];
+      }
+    }
+    return newObj;
+  })
+  return namedChange;
+}
+
