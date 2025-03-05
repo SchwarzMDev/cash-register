730b662e912c4b09a4b749b1d8d4d0328fdbafb7
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-03-05 17:57:15 +0100

    Limit minimum and maximum user input and round it

--- script.js
+++ script.js
@@ -5,33 +5,35 @@ const cashInDrawer = document.querySelector("#cid");
 
 let drawerStatus;
 let totalChange;
-let costumerPaidWith;
-let price = 0;
-let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
+let remainingChange;
+const price = 20;
+
+// cid = cash-in-drawer
+const cid = [
+  ['PENNY', 2.00],
+  ['NICKEL', 2.00],
+  ['DIME', 4.1],
+  ['QUARTER', 7.5],
+  ['ONE', 70],
   ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+  ['TEN', 90],
+  ['TWENTY', 80],
+  ['ONE HUNDRED', 200]
 ];
 
-const moneyValue = {
-  PENNY: 0.01,
-  NICKEL: 0.05,
-  DIME: 0.1,
-  QUARTER: 0.25,
-  ONE: 1,
-  FIVE: 5,
-  TEN: 10,
-  TWENTY: 20,
+const denominations = {
+  'PENNY': 0.01,
+  'NICKEL': 0.05,
+  'DIME': 0.1,
+  'QUARTER': 0.25,
+  'ONE': 1,
+  'FIVE': 5,
+  'TEN': 10,
+  'TWENTY': 20,
   'ONE HUNDRED': 100
 };
-const moneyKeys = Object.keys(moneyValue);
-const moneyValues = Object.values(moneyValue);
+const moneyKeys = Object.keys(denominations);
+const moneyValues = Object.values(denominations);
 
 const statusMessages = {
   'Open': 'Status: OPEN',
@@ -41,9 +43,21 @@ const statusMessages = {
   'No change': 'No change due - customer paid with exact cash'
 };
 
-// look out for negative input!!
 const getUserInput = () => {
-  costumerPaidWith = Number(userInput.value);
+  customerPaidWith = Math.round(Number(userInput.value) * 100) / 100;
+  userInput.value = customerPaidWith;
+  const minDenomination = 0.01;
+  const maxDenomination = price + 100;
+
+  if(customerPaidWith < minDenomination){
+    alert("You haven't paid yet!")
+    userInput.value = "";
+  } else if(customerPaidWith > maxDenomination){
+    alert("Excuse me, you gave me too much!")
+    userInput.value = "";
+  } else {
+    return customerPaidWith;
+  }
 }
 
 const checkTotalChange = () => {
@@ -51,38 +65,50 @@ const checkTotalChange = () => {
     totChange = Math.round((totChange + change[1]) * 100) / 100, 0);
 }
 
+remainingChange = checkTotalChange();
+
 const calcFaceValue = () => {
-  let change = costumerPaidWith - price;
+  const customerPaidWith = getUserInput();
+  const remainingCid = cid.map(el => el);
+  let change = Math.round((customerPaidWith - price) * 100) / 100;
   const faceValues = [];
   checkTotalChange();
 
-  for(i = cid.length - 1; i >= 0; i--){
-    if(change >= moneyValues[i] && cid[i][1] > 0 && totalChange >= change){
+  for(let i = cid.length - 1; i >= 0; i--){
+    if(change >= moneyValues[i] && cid[i][1] > 0 && totalChange >= change && remainingChange >= change){
       change = Math.round((change - moneyValues[i]) * 100) / 100;
       cid[i][1] = Math.round((cid[i][1] - moneyValues[i]) * 100) / 100;
       faceValues.push(moneyValues[i]);
       i++;
-    } 
+    } else {
+      remainingCid.pop();
+      remainingChange = remainingCid.reduce((remChange,change) => 
+        remChange = Math.round((remChange + change[1]) * 100) / 100, 0);
+    }
   }
 
   checkTotalChange();
-  if(change > totalChange){
+  if(change > totalChange || change > 0){
     drawerStatus = 'Insufficient';
-  } else if(costumerPaidWith < price){
+    return [];
+  } else if(customerPaidWith < price){
     drawerStatus = 'Client cannot pay';
-  } else if(costumerPaidWith === price){
+    return [];
+  } else if(customerPaidWith === price){
     drawerStatus = 'No change';
+    return [];
   } else if(totalChange === 0){
     drawerStatus = 'Close';
+    return faceValues;
   } else{
     drawerStatus = 'Open';
+    return faceValues;
   }
-  return faceValues;
+  
 }
 
 const addEqualFaceValues = () => {
   const faceValues = calcFaceValue();
-  console.log(faceValues);
   const change = faceValues.reduce((accumulator, faceValue) => {
     accumulator[faceValue] = (accumulator[faceValue] || 0) + faceValue;
     return accumulator;
@@ -92,17 +118,15 @@ const addEqualFaceValues = () => {
 
 const sortChange = () => {
   const change = addEqualFaceValues();
-  console.log(change);
-  const sortedChange = Object.entries(change).toSorted(([a,], [b,]) => a - b);
+  const sortedChange = Object.entries(change).toSorted(([a,], [b,]) => b - a);
   return sortedChange;
 }
 
 const associateChange = () => {
   const sortedChange = sortChange();
-  console.log(sortedChange);
   const namedChange = sortedChange.map((value) => {
     const newArray = [];
-    for(i = 0; i < moneyValues.length; i++){
+    for(let i = 0; i < moneyValues.length; i++){
       if(value[0] === JSON.stringify(moneyValues[i])){
         newArray.push(moneyKeys[i]);
         newArray.push(value[1]);
@@ -122,13 +146,13 @@ const printChange = () => {
 }
 
 const deleteData = () => {
-  costumerPaidWith = 0;
+  //userInput.value = "";
   changeDue.innerHTML = ``;
   cashInDrawer.innerHTML = ``;
 }
 
 const printCid = () => {
-  for(i = 0; i < cid.length; i++){
+  for(let i = 0; i < cid.length; i++){
     cashInDrawer.innerHTML += `<div>${cid[i][0]}: ${cid[i][1]}</div>`;
     }
 }
@@ -137,7 +161,6 @@ printCid();
 
 purchaseBtn.addEventListener("click", () => {
   deleteData();
-  getUserInput();
   printChange();
   printCid();
 })
